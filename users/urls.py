a712e8a9b0aa2d6a6b1d7ac41d47d6bd58cb33ba
BondarenkoKirill <bku089@gmail.com>
2025-08-16 15:20:43 +0300

    added urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import UserRegistrationView, UserProfileView
 
 urlpatterns = [
-    path("", UserRegistrationView.as_view(), name="user-register"),
+    path("register/", UserRegistrationView.as_view(), name="user-register"),
     path("token/", TokenObtainPairView.as_view(), name="token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("me/", UserProfileView.as_view(), name="user-profile"),
